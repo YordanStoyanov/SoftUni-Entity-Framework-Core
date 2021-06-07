450a8d2892e1b2e20da9153a11a8c3b8b118b014
Yordan Stoyanov <62213580+YordanStoyanov@users.noreply.github.com>
2021-06-07 14:19:20 +0300

    Add files via upload

---
+++ ADO.NET/Exercise/Exercise/Program.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using SqlConnection sqlConnection = new SqlConnection(
+                @"Server=.;Database=MinionsDB;Integrated Security=true");
+            {
+                sqlConnection.Open();
+                string command = @"SELECT [Name] FROM Villains";
+                using SqlCommand reader = new SqlCommand(command, sqlConnection);
+                {
+                    var result = reader.ExecuteReader();
+                    using (result)
+                    {
+                        while (result.Read())
+                        {
+                            string firstName = (string)result[0];
+                            Console.WriteLine(firstName);
+                        }
+                    }
+                        
+                }
+                Console.WriteLine();
+                using SqlCommand dataFromTowns = new SqlCommand("SELECT [Name] FROM Towns", sqlConnection);
+                {
+                    var result2 = dataFromTowns.ExecuteReader();
+                    using (result2)
+                    {
+                        while (result2.Read())
+                        {
+                            string townName = (string)result2[0];
+                            Console.WriteLine(townName);
+                        }
+                    }
+                }
+
+                Console.WriteLine();
+                using SqlCommand dataFromCountries = new SqlCommand("SELECT [Name] FROM Countries", sqlConnection);
+                {
+                    var result3 = dataFromCountries.ExecuteReader();
+                    using (result3)
+                    {
+                        while (result3.Read())
+                        {
+                            var countriesName = (string)result3[0];
+                            Console.WriteLine(countriesName);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ADO.NET/Exercise/HomeWork/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using SqlConnection sqlConnection = new SqlConnection(
+                @"Server=.;Database=MinionsDB;Integrated Security=true");
+            {
+                sqlConnection.Open();
+                string minionsAge = @"SELECT [Name], [Age] FROM Minions ORDER BY [Name] ASC";
+                using SqlCommand reader = new SqlCommand(minionsAge, sqlConnection);
+                {
+                    var result = reader.ExecuteReader();
+                    using (result)
+                    {
+                        while (result.Read())
+                        {
+                            var name = (string)result[0];
+                            var age = (int)result[1];
+                            Console.WriteLine(name + " " + age);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
